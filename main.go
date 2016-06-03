731fbe341f9de385755f3f7b5e88e6ca44c6a9d5
Shantanu <joshishantanu4@gmail.com>
2016-06-03 16:32:05 -0700

    I have stuff running and writing

--- main.go
+++ main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 )
 
 func main() {
 	c := 0
+	tok := "default"
 	for {
 		c++
-		log.Printf("log-test:%d", c)
+		msg := fmt.Sprintf("log-test:%d", c)
+		tok = sendMsg(msg, tok)
+		fmt.Println(msg)
 		time.Sleep(1 * time.Second)
+	}
+}
+
+func sendMsg(msg, tok string) string {
+
+	cl := cloudwatchlogs.New(session.New(awsConfigWithSharedCredentials("us-west-2")))
+	params := &cloudwatchlogs.PutLogEventsInput{
+		LogEvents: []*cloudwatchlogs.InputLogEvent{ // Required
+			{ // Required
+				Message:   aws.String(msg),                     // Required
+				Timestamp: aws.Int64(time.Now().Unix() * 1000), // Required
+			},
+		},
+		LogGroupName:  aws.String("test"),  // Required
+		LogStreamName: aws.String("test2"), // Required
+		SequenceToken: aws.String(tok),
+	}
+
+	resp, err := cl.PutLogEvents(params)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return aws.StringValue(resp.NextSequenceToken)
+}
+
+func awsConfigWithSharedCredentials(region string) *aws.Config {
+	return &aws.Config{
+		Region:      aws.String(region),
+		Credentials: credentials.NewSharedCredentials("", os.Getenv("AWS_PROFILE")),
 	}
 }
